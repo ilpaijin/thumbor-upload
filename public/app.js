fc79f12ea4fd6445c1baac495a4c1d62ba40186d
ilpaijin <ilpaijin@gmail.com>
2015-09-17 15:19:26 +0200

    added data response to view

--- public/app.js
+++ public/app.js
@@ -24,6 +24,7 @@
         evt.preventDefault();
 
         $('#preview').css({'opacity': 0.4}).siblings('.loading').html("<img src='"+spinner+"' />");
+        $('h4.id').find('span').html('');
 
         var formData = new FormData();
         formData.append('file', $('#myImage')[0].files[0]);
@@ -40,9 +41,18 @@
                     return;
                 }
 
-                if (response.data.image) {
+                if (response.data) {
+                    console.info(response.data);
                     $('.upload-outcome-mark > span').addClass('success').fadeIn(1000).css("display","block");
                     $('#preview').css({'opacity': 1}).siblings('.loading').html("");
+
+                    $('h4.id').find('span').html(response.data.id);
+                    $('h4.fileLabel').find('span').html(response.data.label);
+                    $('h4.finename').find('span').html(response.data.filename);
+                    $('h4.width').find('span').html(response.data.width);
+                    $('h4.height').find('span').html(response.data.height);
+                    $('h4.url').find('span').html(response.data.url);
+                    $('h4.created_at').find('span').html(response.data.timestamp);
                 }
             }
         });
